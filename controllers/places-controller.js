4c9c6b0a574ddd8539d20087ec150e9f4130f957
obakengshole <shole.obakeng@gmail.com>
2021-09-21 23:03:11 +0200

    Updated getPlaceByUserId to getPlaces... to accommodate for multiple places

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -29,20 +29,20 @@ const getPlaceById = (req, res, next) => {
     res.json({ place })
 }
 
-const getPlaceByUserId = (req, res, next) => {
+const getPlacesByUserId = (req, res, next) => {
     const userId = req.params.userId
-    const place = DUMMY_PLACES.find(place => {
+    const places = DUMMY_PLACES.filter(place => {
         return place.creator === userId
     })
 
-    if (!place) {
+    if (!places || places.length === 0) {
         // return res.status(404).json({ message: 'Could not find a place for the provided user id.' })
         // const error = new Error('Could not find a place for the provided user id.')
         // error.code = 404
-        return next(new HttpError('Could not find a place for the provided user id.', 404))
+        return next(new HttpError('Could not find a places for the provided user id.', 404))
     }
 
-    res.json({ place })
+    res.json({ places })
 }
 
 const createPlace = (req, res, next) => {
@@ -82,7 +82,7 @@ const deletePlace = (req, res, next) => {
 }
 
 exports.getPlaceById = getPlaceById
-exports.getPlaceByUserId = getPlaceByUserId
+exports.getPlacesByUserId = getPlacesByUserId
 exports.createPlace = createPlace
 exports.updatePlace = updatePlace
 exports.deletePlace = deletePlace
