aa675febb4cdf14f53f45c38653d23a0232968df
obakengshole <shole.obakeng@gmail.com>
2021-10-07 11:16:42 +0200

    updated updatePlace function to use mongoose function

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -91,7 +91,7 @@ const createPlace = async (req, res, next) => {
     res.status(201).json({ place: createdPlace }) // status code setnt if something was created on the server
 }
 
-const updatePlace = (req, res, next) => {
+const updatePlace = async (req, res, next) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
         throw new HttpError('Invalid input passed.', 422)
@@ -100,14 +100,27 @@ const updatePlace = (req, res, next) => {
     const { title, description } = req.body
     const placeId = req.params.placeId
 
-    const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) }
-    const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId)
-    updatedPlace.title = title
-    updatedPlace.description = description
+    let place
+    try {
+        place = await Place.findById(placeId)
+    } catch (err) {
+        const error = new HttpError('Could not update place because place not found, please try again.', 404)
+        console.log(err);
+        return next(error)
+    }
 
-    DUMMY_PLACES[placeIndex] = updatedPlace
+    place.title = title
+    place.description = description
 
-    res.status(200).json({ place: updatedPlace })
+    try {
+        await place.save()
+    } catch (err) {
+        const error = new HttpError('Creating place failed, please try again.', 500)
+        console.log(err);
+        return next(error)
+    }
+
+    res.status(200).json({ place: place.toObject({ getters: true }) })
 }
 
 const deletePlace = (req, res, next) => {
