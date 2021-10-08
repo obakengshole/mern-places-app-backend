9fd58559670d908de23ad2bcf575c53b25cdcf0c
obakengshole <shole.obakeng@gmail.com>
2021-10-08 13:42:00 +0200

    Added users schema and  mongoose functionality to signup method

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -65,7 +65,9 @@ const createPlace = async (req, res, next) => {
     const errors = validationResult(req)
     console.log(errors);
     if (!errors.isEmpty()) {
-        throw new HttpError('Invalid input passed.', 422)
+        return next(
+            new HttpError('Invalid input passed.', 422)
+        )
     }
 
     const { title, description, coordinates, address, creator } = req.body
@@ -94,7 +96,9 @@ const createPlace = async (req, res, next) => {
 const updatePlace = async (req, res, next) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
-        throw new HttpError('Invalid input passed.', 422)
+        return next(
+            new HttpError('Invalid input passed.', 422)
+        )
     }
     
     const { title, description } = req.body
@@ -123,13 +127,24 @@ const updatePlace = async (req, res, next) => {
     res.status(200).json({ place: place.toObject({ getters: true }) })
 }
 
-const deletePlace = (req, res, next) => {
+const deletePlace = async (req, res, next) => {
     const placeId = req.params.placeId
-    if (!DUMMY_PLACES.find(place => place.id === placeId)) {
-        throw new HttpError('Could not find a place for that id.', 404)
+
+    let place
+    try {
+        place = await Place.findById(placeId)
+    } catch (err) {
+        const error = new HttpError('Could not delete place, place not found.', 500)
+        return next(error)
     }
-    
-    DUMMY_PLACES = DUMMY_PLACES.filter(place => place.id !== placeId)
+
+    try {
+        await place.remove()
+    } catch (err) {
+        const error = new HttpError('Could not delete place.', 500)
+        return next(error)
+    }
+
     res.status(200).json({ message: 'Deleted place.' })
 }
 
--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,7 +1,9 @@
-// const uuid = require('uuid/v4')
+const uuid = require('uuid/v4')
 
 const HttpError = require('../models/http-error')
 const { validationResult } = require('express-validator')
+
+const User = require('../models/user')
 
 const DUMMY_USERS = [
     {
@@ -16,36 +18,56 @@ const getUsers = (req, res, next) => {
     res.json({ users: DUMMY_USERS })
 }
 
-const signup = (req, res, next) => {
+const signup = async (req, res, next) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
-        throw new HttpError('Invalid input passed.', 422)
+        return next(
+            new HttpError('Invalid input passed.', 422)
+        )
     }
 
-    const { name, email, password } = req.body
+    const { name, email, password, places } = req.body
 
-    const hasUser = DUMMY_USERS.find(user => user.email === email)
-    if (hasUser) {
-        throw new HttpError('Could not create user, email already exists.', 422)
+    let existingUser
+    try {
+        // find one document matching the criteria in the argument of our method
+        existingUser = await User.findOne({ email: email })
+    } catch (err) {
+        const error = new HttpError('Signup failed, please try again later.', 500)
+        return next(error)
     }
 
-    const createdUser = {
-        id: 123,
+    if (existingUser) {
+        const error = new HttpError('User already exists. Please login instead', 422)
+        return next(error)
+    }
+
+    const createdUser = new User({
         name,
         email,
-        password
-    }
+        image: 'https://live.staticflickr.com/7631/26849088292_36fc52ee90_b.jpg',
+        password,
+        places
+    })
 
-    DUMMY_USERS.push(createdUser)
+    try {
+        await createdUser.save()
+    } catch (err) {
+        const error = new HttpError('Signup failed, please try again later.', 500)
+        return next(error)
+    } 
 
-    res.status(201).json({ user: createdUser })
+    res.status(201).json({ user: createdUser.toObject({ getters: true }) })
 }
+
 const login = (req, res, next) => {
     const { email, password } = req.body
 
     const identifiedUser = DUMMY_USERS.find(user => user.email === email)
     if (!identifiedUser) {
-        throw new HttpError('Could not identify user. Check credentials.', 401)
+        return next(
+            new HttpError('Could not identify user. Check credentials.', 401)
+        )
     }
 
     res.json({ message: 'Logged in'})
